8b796381a82ebbc4905f20918900aca119132f8e
Gabriele.Liporace <gabriele.liporace@edu.unito.it>
2022-05-31 20:18:25 +0200

    corretta terminazione dei processi e inserito conto dei retry

--- master.c
+++ master.c
@@ -19,6 +19,16 @@ void my_handler(int signum)
     }
 }
 
+int get_casual_pid(pid_t * array,int un){ 
+    struct timespec spec;
+    int rec,pid;
+    clock_gettime(CLOCK_REALTIME,&spec);
+    srand(spec.tv_nsec);
+    rec = rand() % un;
+    pid = array[rec];
+    return pid;
+}  
+
 int  inizializzazione_valori()
 {
 	/*Creazione degli stream*/
@@ -42,34 +52,42 @@ int  inizializzazione_valori()
 
     fclose(masterbook_init);
 }
-
+/*inizio main*/
 int main(int argc, char *argv[])
 { 
-	int utenti,nodi,bilancio,creo_transazione,count,status,t_attesa;
-    int sem_id;
-    char * id_transazione;
+	int utenti,nodi,bilancio,count,status,t_attesa;
+    int sender,reciver,rec;
+    pid_t u;
+    int vivi;
+    int myretry;
+    int msg_id;
+    int sem_id;  
+    char * transaction;
 	long Bill;
+    pid_t miopid;
     pid_t * array_utenti,pid_user; /*Tipo integer e rappresenta l'id del processo*/
     pid_t * array_nodi,pid_nod;
+    struct timespec spec;
     struct sigaction sa; /*Gestione dei segnali*/
     struct sembuf sops; /*Struct per operazioni sui semafori*/
+
     inizializzazione_valori(); /*Richiamo la funzione per le variabili globali*/
-    printf("my pid: %d\n",getpid());
     bzero(&sa, sizeof(sa));
     sa.sa_handler=my_handler;
-    sigaction(SIGALRM,&sa,NULL);
     sigaction(SIGUSR1,&sa,NULL);
 
-
+    /*creazioe coda di messaggi per mandare la transazione*/
+    msg_id = msgget(MSG_KEY,IPC_CREAT | 0600 );
     /* creazione di semaforo per la sincronizzazione dei processi */
 	sem_id=semget(SEM_KEY,3,IPC_CREAT|0600);
 	semctl(sem_id,0,SETVAL,0);
 	semctl(sem_id,1,SETVAL,0);
 	semctl(sem_id,2,SETVAL,0);
 
+    miopid=getpid();
     sops.sem_flg=0;
 
-	/*
+    /*
     Dichiarazione array per PID utenti e PID nodi
     */
 
@@ -80,59 +98,12 @@ int main(int argc, char *argv[])
      Creo con fork() i processi utente
      */
 
-    for(utenti=0; utenti<SO_USERS_NUM;utenti++){
-        switch(pid_user=fork())
-          {
-          case -1:
-             exit(EXIT_FAILURE);
-          case 0:
-
-            /*La funzione free() dealloca il bloccco di memoria preallocato dalla malloc*/
-            free(array_utenti);
-
-            /*Semaforo per inizializzazione array_utenti*/
-            sops.sem_num=0;	
-            sops.sem_op=1;
-            semop(sem_id,&sops,1);
-
-            /*Blocco per sincronizzare con l'avvio del timer*/
-            sops.sem_num=2;
-            sops.sem_op=1;
-            semop(sem_id,&sops,1);
-            
-            printf("Utente #%d\n",utenti);
-/*
-			calcolo l'attesa
-			t_attesa = get_attesa(SO_MAX_TRANS_GEN_NSEC,SO_MIN_TRANS_GEN_NSEC);
-			calcolo il bilancio
-			bilancio = get_balance(SO_BUDGET_INIT); 
-			creo l'id della transazione
-			id_transazione = id(array_utenti,pid_user);
-			creo la transazione
-			creo_transazione = all_utenti(SO_REWARD,bilancio,id_transazione);
-			printf("l'id della transazione e' %s \n",id_transazione);
-			usleep(t_attesa);
-			exit(0);
-*/
-            if (utenti == 10 || utenti==19){
-                sleep(5);
-            }else{
-                sleep(35);
-            }
-            exit(0);
-            break;
-
-            default:
-                /*inserisce nell'array utenti il pid del processo figlio*/
-                array_utenti[utenti]=pid_user;
-                break;
-        }
-    }
-
     /*
     creo i processi nodo con fork()
     */
-    for(nodi = 0; nodi<=SO_NODES_NUM;nodi++){
+    
+    printf("miopid: %d\n", getpid());
+    for(nodi = 0; nodi<SO_NODES_NUM;nodi++){
         switch (pid_nod=fork())
         {
         case -1:
@@ -140,6 +111,7 @@ int main(int argc, char *argv[])
         case 0:
 
 		    free(array_nodi);
+            alarm(SO_SIM_SEC);
 
             /*Semaforo per inizializzazione array_utenti*/
             sops.sem_num=1;	
@@ -151,7 +123,7 @@ int main(int argc, char *argv[])
             sops.sem_op=1;
             semop(sem_id,&sops,1);
 
-            printf("Nodo #%d\n",nodi);
+            /*printf("Nodo #%d\n",nodi);*/
             sleep(30);
             exit(0);
 
@@ -160,6 +132,61 @@ int main(int argc, char *argv[])
              break;
         }
     }
+
+    for(utenti=0; utenti<SO_USERS_NUM;utenti++){
+        switch(pid_user=fork())
+          {
+          case -1:
+             exit(EXIT_FAILURE);
+          case 0:
+            /*La funzione free() dealloca il bloccco di memoria preallocato dalla malloc*/
+            alarm(SO_SIM_SEC);
+
+            /*Semaforo per inizializzazione array_utenti*/
+            sops.sem_num=0;	
+            sops.sem_op=1;
+            semop(sem_id,&sops,1);
+
+            /*Blocco per sincronizzare con l'avvio del timer*/
+            sops.sem_num=2;
+            sops.sem_op=1;
+            semop(sem_id,&sops,1);
+            
+            /*printf("Utente #%d\n",utenti);*/
+
+            myretry=SO_RETRY;
+            while(myretry){
+            /*creazione transazione*/
+            /*bilancio = get_balance(SO_BUDGET_INIT);*/
+#ifdef BIL
+                bilancio = 2;
+#else
+                bilancio = 1;
+#endif
+                if(bilancio >= 2){
+                    myretry=SO_RETRY;
+                    sender = getpid();
+                    reciver = get_casual_pid(array_utenti,utenti);
+                    printf("il reciver e' %d\n",reciver);
+                    transaction = creazione_transazione(SO_REWARD,bilancio,reciver,sender);
+                    printf("la transazione e' %s \n",transaction);
+                    sleep(1);
+                }else if(bilancio < 2){
+                    myretry--;
+                    sleep(1);
+                }
+            }
+			exit(0);
+            break;
+
+            default:
+                /*inserisce nell'array utenti il pid del processo figlio*/
+                array_utenti[utenti]=pid_user;
+
+                break;
+        }
+    }
+
 
     /*Lascio partire gli utenti*/
 	sops.sem_num=0;	
@@ -176,51 +203,45 @@ int main(int argc, char *argv[])
 	sops.sem_op=-(utenti+nodi);
 	semop(sem_id,&sops,1);
 
-    alarm(SO_SIM_SEC);
+    sigaction(SIGALRM,&sa,NULL);
+    printf("utenti %d\n", utenti);
+    vivi=utenti;
 
-    while(i){
-
-        
-        /* verifico che i pid che mi ritornino siano quelli utente,
-         * cosi' quando a zero termino
-         */
-
-        /*for(count=0; count<SO_USERS_NUM;count++){*/
-        while(waitpid(array_utenti[utenti],&status,WUNTRACED | WCONTINUED)!= -1){
-             /*
-             * attendo la terminazione di tutti i processi utente
-             */ 
-
-            /*controllo causa uscita e riduco di uno gli utenti attivi*/
-            if(WIFEXITED(status)){
-                utenti--;
-            }
-            if(WIFSIGNALED(status)){
-                utenti--;
-            }   
-            printf("utenti rimasti: %d\n", utenti);
-
-            /*alzo un sigalrm che setta i a 0*/
-            raise(SIGALRM);
-        }
-        /*}*/
-
+    for (count=0; count < nodi; count++){
+        printf("nodo: %d\n", array_nodi[count]);
     }
 
-    sigaction(SIGTERM,&sa,NULL);
-
-    /*Termino tutti gli utenti rimasti*/
     for (count=0; count < utenti; count++){
-        kill(array_utenti[utenti], SIGTERM);
+        printf("utente: %d\n", array_utenti[count]);
+    }
+
+    while((u=wait(&status)) != -1){
+    
+        for(count=0; count < utenti; count++){
+            if(u==array_utenti[count]){
+                vivi--;
+            }
+            
+        }
+        if(vivi == 0){
+            /*Termino i nodi se gli utenti sono a zero*/
+            for (count=0; count < nodi; count++){
+                kill(array_nodi[count], SIGTERM);
+            }
+            
+        }
+        printf("processo terminato: %d , %d, %d , %d\n", WIFEXITED(status), WIFSIGNALED(status), u, vivi);
     }
     
-    /*Termino tutti i nodi rimasti*/
-    for (count=0; count < nodi; count++){
-        kill(array_nodi[nodi], SIGTERM);
-    }
+    sigaction(SIGTERM,&sa,NULL);
+    printf("nodi %d\n", nodi);
+    printf("miopid %d\n", getpid());
+
+    printf("ciao ciao \n");
 	/*Rimuovo il semaforo*/
 	semctl(sem_id,0,IPC_RMID);
 
+    msgctl(msg_id, IPC_RMID, NULL);
     free(array_utenti);
     free(array_nodi);
 
--- master.h
+++ master.h
@@ -8,24 +8,27 @@
 #include <sys/shm.h>
 #include <sys/sem.h>
 #include <sys/types.h>
+#include <sys/msg.h>
 #include <signal.h>
-#include <errno.h>	
-#include <time.h>	
+#include <errno.h>
+#include <time.h>
+#include <semaphore.h>
+ 
 
-/* definizione di macro */
+/* definizione di macro*/
 #define SO_BLOCK_SIZE 10
 #define SO_REGISTRY_SIZE 1000
 
-/* definizione semaphore key */
+/*definizione chiavi semafori e code di messaggi*/
 #define SEM_KEY 0x113522
+#define MSG_KEY 0x229729
 
 /* dichiarazione di variabili globali*/
 int SO_USERS_NUM, SO_NODES_NUM, SO_BUDGET_INIT, SO_REWARD;
 int SO_MIN_TRANS_GEN_NSEC,SO_MAX_TRANS_GEN_NSEC,SO_RETRY,SO_TP_SIZE;
 int SO_MIN_TRANS_PROC_NSEC,SO_MAX_TRANS_PROC_NSEC,SO_SIM_SEC;
-int i=1;
+int i = 1;
 
-/*definizione funzioni*/
 
 int inizializzazione_valori(); /*Legge i valori dal file init e li assegna*/
 
